6b3e8c54b97d59e4215fbbb9d04ca9bdcf285749
Raghavendrapara <raghavendra0360@gmail.com>
2020-06-08 09:54:03 +0530

    Structural changes

---
+++ Tracking/src/imag/ImageLoader.java
@@ -0,0 +1,88 @@
+package imag;
+
+import java.io.File;
+import java.util.*;
+import ij.IJ;
+import ij.ImagePlus;
+import ij.ImageStack;
+import ij.gui.Roi;
+import java.util.regex.*;
+public class ImageLoader {
+	private List<String> images;
+	public void main(String[] args) {
+		// TODO Auto-generated method stub
+        new ImageJ();
+		int x=loadImages("/home/raghavendra/Downloads/PhC-C2DH-U373/01_ST");
+		
+		System.out.println(x);
+	}
+
+private int loadImages(String directory)
+{
+	this.images.clear();
+	File folder = new File(directory);
+	File[] images = sortImages(folder.listFiles());
+
+	for (File file : images) {
+		if (file.isFile()) {
+			this.images.add(file.getName());
+		}
+	}
+	return this.images.size();
+}
+
+private File[] sortImages(File[] images) {
+	final Pattern p = Pattern.compile("\\d+");
+	Arrays.sort(images, new  Comparator<File>(){
+		@Override public int compare(File o1, File o2) {
+			Matcher m = p.matcher(o1.getName());
+			Integer number1 = null;
+			if (!m.find()) {
+				return o1.getName().compareTo(o2.getName());
+			}
+			else {
+				Integer number2 = null;
+				number1 = Integer.parseInt(m.group());
+				m = p.matcher(o2.getName());
+				if (!m.find()) {
+					return o1.getName().compareTo(o2.getName());
+				}
+				else {
+					number2 = Integer.parseInt(m.group());
+					int comparison = number1.compareTo(number2);
+					if (comparison != 0) {
+						return comparison;
+					}
+					else {
+						return o1.getName().compareTo(o2.getName());
+					}
+				}
+			}
+		}}
+			);
+	return images;
+}
+
+
+private ImageStack loadFeatureStack(String imageName){
+	String localPath=imageName.substring(0, imageName.lastIndexOf("."));
+	//System.out.println(featurePath+images.get(0).substring(0, images.get(0).lastIndexOf(".")));
+
+	
+	File[] images=sortImages(new File(localPath).listFiles());
+	ImagePlus firstImage=IJ.openImage(localPath+"/"+images[0].getName());
+	ImageStack featureStack = new ImageStack(firstImage.getWidth(), firstImage.getHeight());
+	for(File file : images){
+		if (file.isFile()) {
+			//System.out.println(file.getName());
+			IJ.log(file.getName());
+			ImagePlus image=IJ.openImage(localPath+"/"+file.getName());
+
+			featureStack.addSlice(image.getTitle(), image.getProcessor());
+
+		}
+	}
+	return featureStack;
+}
+
+}
