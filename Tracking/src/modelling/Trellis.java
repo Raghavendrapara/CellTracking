7a8a564d07ecc1d2aeb696c44cd693d64e08735b
Raghavendrapara <raghavendra0360@gmail.com>
2020-06-20 10:14:01 +0530

    State Diagram and Viterbi Abstract Implementation based on pseudo objects Node and Arc

--- Tracking/src/modelling/Trellis.java
+++ Tracking/src/modelling/Trellis.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 public class Trellis
 {
 // Default constructor used to make it possible to inherit from the class.
-	ArrayList<ArrayList<Nodes>> mNodes;
-    int mNumT;
+ArrayList<ArrayList<Nodes>> mNodes;//Layer number wise storage of Nodes for each tiff image 
+int mNumT;//No. Of tiff images/layers
 Trellis(int aNumT)
 {
 	mNumT=aNumT;
@@ -16,9 +16,10 @@ Trellis(int aNumT)
 	}
 }
 
-//Implement a destructor type for trellis
+//Implement a destructor equivalent for trellis
 
-void addNode(int aT, Node aNode) {
+void addNode(int aT, Node aNode) //aT denotes position of the image in tiff stack 
+{
 	ArrayList<Node> arr=mNodes.get(aT);
 	arr.add(aNode);
 	mNodes.set(aT,arr);
@@ -26,25 +27,29 @@ void addNode(int aT, Node aNode) {
 
 Node GetNode(int aT, int aN) 
 {
-	return mNodes.get(aT).get(aN);
+	return mNodes.get(aT).get(aN);//Layer Number aT and Node number aN
 }
 
-void HighestScoringPath(Arraylist<Arc> Arcs, double aScore) {
+void HighestScoringPath(Arraylist<Arc> Arcs, double aScore) //Implementation of the pseudo code for Viterbi Algorithm
+{
 
-	ArrayList<ArrayList<Arc>> bestArcs;		
+	ArrayList<ArrayList<Arc>> bestArcs;		//Arc simply denotes the edges carrying the same naming as used in the paperper
 	ArrayList<ArrayList<Double>> bestScores;
 	ArrayList<ArrayList<Integer>> prevIndex;		
 
-	for (int t=0; t<mNumT; t++) {
+	for (int t=0; t<mNumT; t++) 
+	{
 		bestArcs.add(new ArrayList<Arc>(mNodes.size()));
 		ArrayList<Double> x1=new ArrayList<Double>(mNodes.get(t).size());
 		ArrayList<Integer> x2=new ArrayList<Integer>(mNodes.get(t).size());
-		for(int i=0;i<x.size();i++)
-		{
-			x1.set(i,Integer.MAX_VALUE);
-			x2.set(index, -1);
-	    }
-	    bestScores.add(x1);
+		
+		for(int i=0;i<x1.size();i++)
+		    {
+			x1.set(i,-(Integer.MAX_VALUE));
+			x2.set(i, -1);
+	            }
+	        
+		bestScores.add(x1);
 		prevIndex.add(x2);  // -1 indicates that the node can not be reached.
 	}
 
@@ -55,44 +60,52 @@ void HighestScoringPath(Arraylist<Arc> Arcs, double aScore) {
     }
     bestScores.set(0,d);
 	// Go through the layers one by one to find the highest scoring path from the beginning of the Trellis to the end.
-    for (int t=1; t<mNumT; t++) {
-        for (int n=0; n<mNodes.get(t).size(); n++) {
-            Node node = mNodes.get(t).get(n);
-			for (int i=0; i<node.GetNumBackwardArcs(); i++) {
-				Arc bArc = node.GetBackwardArc(i);
-                int pIndex = bArc.GetStartNode().GetIndex();
-                double score = bestScores.get(t-1).get(pIndex) + bArc.GetScore();
-                if (i==0 || score > bestScores.get(t).get(n))
-                {
+    for (int t=1; t<mNumT; t++) 
+    {
+        for (int n=0; n<mNodes.get(t).size(); n++)
+	{
+             Node node = mNodes.get(t).get(n);
+		
+		for (int i=0; i<node.GetNumBackwardArcs(); i++) 
+		{
+			  Arc bArc     = node.GetBackwardArc(i);
+                          int pIndex   = bArc.GetStartNode().GetIndex();
+                          double score = bestScores.get(t-1).get(pIndex) + bArc.GetScore();
+                          if (i==0 || score > bestScores.get(t).get(n))
+                          {
                 	
-                    ArrayList<Arc> y1=bestArcs.get(t);
-                    y1.set(n,bArc);
-                    bestArcs.set(t,y1);
+                          ArrayList<Arc> y1=bestArcs.get(t);
+                          y1.set(n,bArc);
+                          bestArcs.set(t,y1);
                     
-                    ArrayList<Double> y2=bestScores.get(t);
-                    y2.set(n,score);
-                    bestScores.set(t,y2);
+                          ArrayList<Double> y2=bestScores.get(t);
+                          y2.set(n,score);
+                          bestScores.set(t,y2);
 
-                    ArrayList<Integer> y3=prevIndex.get(t);
-                    y3.set(n,pIndex);
-                    bestArcs.set(t,y3;
+                          ArrayList<Integer> y3=prevIndex.get(t);
+                          y3.set(n,pIndex);
+                          bestArcs.set(t,y3;
+         
+		          }
                 }
-            }
         }
     }
-    
+    //BackTracking
    	int endIndex = 0;
-	for (int n=0; n<mNodes.get(mNumT-1).size();n++){
-		if (bestScores.get(mNumT-1).get(n) > bestScores.get(mNumT-1).get(endIndex)) {
+	for (int n=0; n<mNodes.get(mNumT-1).size();n++)
+	{
+		if (bestScores.get(mNumT-1).get(n) > bestScores.get(mNumT-1).get(endIndex)) 
+		{
 			endIndex = n;
 		}
 	}
 
 	int maxIndex = endIndex;
-    for (int t=mNumT-1; t>0; t--) {
-		aArcs.add(bestArcs.get(t).get(maxIndex));
+        for (int t=mNumT-1; t>0; t--)
+        {
+	aArcs.add(bestArcs.get(t).get(maxIndex));
         maxIndex = prevIndex.get(t).get(maxIndex);
-    }
+        }
 
 	// Set output score.
     aScore = bestScores.get(mNumT-1).get(endIndex);
