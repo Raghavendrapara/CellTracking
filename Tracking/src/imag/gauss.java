a0fd55c2f466ba0e0968cd0bd6ee5fa1955ec56d
Raghavendrapara <raghavendra0360@gmail.com>
2020-07-01 18:10:30 +0530

    Trial with Logistic

--- Tracking/src/imag/gauss.java
+++ Tracking/src/imag/gauss.java
@@ -1,10 +1,29 @@
 package imag;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.apache.commons.math3.analysis.function.Gaussian;
+
+import weka.classifiers.functions.LinearRegression;
+import weka.classifiers.functions.Logistic;
+import weka.classifiers.functions.SimpleLogistic;
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
 public class gauss {
-public static void main(String args[])
+public static void main(String args[]) throws Exception
 {
-	Gaussian v=new Gaussian(1,1);
-	double x=v.value(3.3);
-	System.out.println(x);
+	DataSource source = new DataSource("/home/raghavendra/Desktop/diabetes.arff");
+	 Instances instances = source.getDataSet();
+
+	 instances.setClassIndex(instances.numAttributes() - 1);
+	 System.out.println("\nDataset:\n");
+	 System.out.println(instances);
+SimpleLogistic ob=new SimpleLogistic();
+ob.buildClassifier(instances);
+//double[][] x=ob.coefficients();
+System.out.print(ob.toString());
 }
+
 }
